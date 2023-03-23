03777ee0474a75c6b12ef29b4a9a2b5769e2bf5a
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-03-23 13:49:12 -0400

    Started the main function

--- SRC/main.c
+++ SRC/main.c
@@ -6,13 +6,20 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:42:14 by apereira          #+#    #+#             */
-/*   Updated: 2023/03/23 13:43:02 by apereira         ###   ########.fr       */
+/*   Updated: 2023/03/23 13:49:07 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envv)
 {
+	char	*input;
+	char	**commands;
 
+	while (1)
+	{
+		
+	}
+	return (0);
 }
