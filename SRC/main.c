4c36846b502be4430239190c6ddb2b6d8a53e228
Abel Tiago Pereira <apereira@c2r7s1.42lisboa.com>
2023-03-28 15:13:00 +0100

    aaa

--- SRC/main.c
+++ SRC/main.c
@@ -6,16 +6,16 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:42:14 by apereira          #+#    #+#             */
-/*   Updated: 2023/03/28 14:29:31 by apereira         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:12:40 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	main()
+int	main(int argc, char **argv, char **env)
 {
 	char	*input;
-	// char	**commands;
+	char	**commands;
 
 	while (1)
 	{
