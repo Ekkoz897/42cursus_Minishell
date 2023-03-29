6f60254c8f6426b7d7608c4dbbb5f5263dfefc15
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-03-29 11:11:55 -0400

    malloc right ammount for commands

--- SRC/main.c
+++ SRC/main.c
@@ -6,11 +6,53 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:42:14 by apereira          #+#    #+#             */
-/*   Updated: 2023/03/29 10:24:35 by apereira         ###   ########.fr       */
+/*   Updated: 2023/03/29 11:11:41 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+
+// Function takes the input string and splits the commands into one array each 
+// their with flags
+char	**ft_split_cmds(char *input)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**cmds;
+
+	i = 0;
+	cmds = ft_calloc(1, sizeof(char *));
+	while (input[i])
+	{
+		if (input[i] == '|')
+		{
+			free (cmds);
+			cmds = ft_calloc(3, sizeof(char *));
+			j = i;
+			cmds[0] = ft_calloc(j + 1, sizeof(char));
+			while (j >= 0)
+			{
+				cmds[0][j] = input[j];
+				j--;
+			}
+			cmds[1] = ft_calloc(2, sizeof(char));
+			cmds[1][0] = '|';
+			while (input[i++])
+				j++;
+			cmds[2] = ft_calloc(j, sizeof(char));
+			while (j >= 0)
+			{
+				cmds[2][j] = input[i];
+				j--;
+				i--;
+			}
+		}
+		i++;
+	}
+	
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -29,6 +71,7 @@ int	main(int ac, char **av, char **env)
 			break ;
 		}
 		add_history(input);
+		
 		ft_printf("You entered: %s\n", input);
 		free(input);
 	}
