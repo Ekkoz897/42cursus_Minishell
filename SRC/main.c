ac1ffb79675cfe591b1e4191e61b5d282d638b60
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-04-03 11:18:08 -0400

    no seg

--- SRC/main.c
+++ SRC/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: miandrad <miandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:42:14 by apereira          #+#    #+#             */
-/*   Updated: 2023/04/03 11:07:37 by apereira         ###   ########.fr       */
+/*   Updated: 2023/04/03 11:17:56 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(int ac, char **av, char **env)
 {
+	int		i;
 	char	*input;
 	char	**commands;
 	t_vars	vars;
 	int		pipe_fd[2];
 
+	i = 0;
 	(void)ac;
 	(void)av;
 	while (1)
@@ -32,13 +34,12 @@ int	main(int ac, char **av, char **env)
 		add_history(input);
 		ft_printf("You entered: %s\n", input);
 		commands = ft_split_commands(input, "|");
-		if (commands)
-			ft_printf("cmd[0] = %s\n", commands[0]);
+		while (commands[i])
+		{
+			ft_printf("cmd[%i] : %s\n", i, commands[i]);
+			i++;
+		}
 		vars.cmd1_flags = ft_split(commands[0], ' ');
-		if (commands[1])
-			ft_printf("cmd[1] = %s\n", commands[1]);
-		if (commands[2])
-			ft_printf("cmd[2] = %s\n", commands[2]);
 		first_process(&vars, env, pipe_fd);
 		ft_free(commands);
 		ft_free(vars.cmd1_flags);
