14506eecf589923b97244d1b189da0d7ec234183
Ekkoz <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-05-02 15:45:21 -0400

    Norminetted everything and merged with the heredoc code. Signals finally working properly for every case.

--- SRC/main.c
+++ SRC/main.c
@@ -6,11 +6,61 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:42:14 by apereira          #+#    #+#             */
-/*   Updated: 2023/04/27 12:15:50 by apereira         ###   ########.fr       */
+/*   Updated: 2023/05/02 15:43:50 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	open_doc(t_vars *vars, char *commands, int *j)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	commands = ft_strrchr(commands, '<');
+	commands++;
+	while (*commands == ' ' || *commands == '	')
+		commands++;
+	while (commands[i] != ' ' && commands[i] != '	' && commands[i])
+		i++;
+	vars->here_doc_fd[*j] = open("./TMP", __O_TMPFILE | O_RDWR, 0000644);
+	str = get_next_line(0);
+	while (ft_strncmp(str, commands, ft_strlen(str)) != 0)
+	{
+		free(str);
+		str = get_next_line(0);
+	}
+	free(str);
+	(*j)++;
+}
+
+void	here_doc(t_vars *vars, char **commands)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (commands[i])
+	{
+		if (ft_strrchr(commands[i], '<') && *(ft_strrchr(commands[i], \
+			'<') - 1) == '<')
+			j++;
+		i++;
+	}
+	vars->here_doc_fd = malloc(sizeof(int) * j + 1);
+	vars->here_doc_fd[j] = '\0';
+	i = 0;
+	j = 0;
+	while (commands[i])
+	{
+		if (ft_strrchr(commands[i], '<') && *(ft_strrchr(commands[i], \
+			'<') - 1) == '<')
+			open_doc(vars, commands[i], &j);
+		i++;
+	}
+}
 
 // To print the commands stored in the array
 	// if (commands)
@@ -28,6 +78,7 @@ void	minishell(char *input, char **env)
 	int		pipe_fd[2];
 
 	commands = ft_split_commands(input, "|");
+	here_doc(&vars, commands);
 	i = 0;
 	vars.p0 = 0;
 	while (commands[i])
@@ -56,10 +107,10 @@ int	main(int ac, char **av, char **env)
 	(void)av;
 	rl_catch_signals = 0;
 	rl_set_signals();
-	signal(SIGQUIT, signal_handler);
-	signal(SIGINT, signal_handler);
 	while (1)
 	{
+		signal(SIGQUIT, signal_handler);
+		signal(SIGINT, signal_handler);
 		input = readline("myshell> ");
 		if (!input)
 		{
@@ -69,6 +120,8 @@ int	main(int ac, char **av, char **env)
 		}
 		if (ft_strlen(input) != 0)
 			add_history(input);
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, custom_handle_signal);
 		if (ft_strlen(input) != 0)
 			minishell(input, env);
 	}
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:40:10 by apereira          #+#    #+#             */
-/*   Updated: 2023/04/26 11:27:34 by apereira         ###   ########.fr       */
+/*   Updated: 2023/05/02 15:44:57 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft/libft.h"
+# include "GNL/get_next_line.h"
 # include "ft_printf/ft_printf.h"
 
 // Same struct used in Pipex
@@ -35,14 +36,15 @@ typedef struct s_vars
 	char	*cmd2_path;
 	char	**cmd_flags;
 	char	**cmd2_flags;
+	int		*here_doc_fd;
 }t_vars;
 
 // Struct for commands list
-typedef struct s_cmds
-{
-	void			**content;
-	struct s_cmds	*next;
-}t_cmds;
+// typedef struct s_cmds
+// {
+// 	void			**content;
+// 	struct s_cmds	*next;
+// }t_cmds;
 
 // Input Sanitize.c
 char		*check_valid_cmd(char *argv, char **envp);
@@ -64,8 +66,8 @@ void		first_process(t_vars *vars, char **envp, int *pipe_fd,
 
 // utils.c
 char		*find_path(char **envp);
-void		ft_free(char **array);
 int			count_words(const char *str, const char *delimiters);
+void		custom_handle_signal(int sign);
 
 // util2.c
 char		**ft_split_commands_no_redirection(char *str,
