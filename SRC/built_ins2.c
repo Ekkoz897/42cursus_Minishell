6f7a911a0e4377731d50574837e0fd36759f74d8
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-07-13 11:29:51 -0400

    Merge sputid

--- SRC/built_ins2.c
+++ SRC/built_ins2.c
@@ -6,7 +6,7 @@
 /*   By: miandrad <miandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 11:27:54 by apereira          #+#    #+#             */
-/*   Updated: 2023/07/11 13:51:07 by miandrad         ###   ########.fr       */
+/*   Updated: 2023/07/13 11:27:17 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,11 +94,12 @@ char	*get_env_var(t_vars *vars, const char *name)
 
 // static to stay within 25 lines without spliting such a simple
 // function in two.
-char	**copy_environ(char **environ)
+void	copy_environ(char **environ, t_vars *vars)
 {
 	int		i;
 	int		j;
 
+	i = 0;
 	while (environ[i] != NULL)
 		i++;
 	vars->num_env_vars = i;
